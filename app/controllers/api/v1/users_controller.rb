38c674f478b446ced07fd0c6a5195c4d54230215
Matt Migliore <mmigliore311@gmail.com>
2018-12-31 10:22:20 -0500

    fix spacing in users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    @user =  User.create(user_params)
+    @user = User.create(user_params)
     @user.save
     render json: @user , status: :accepted
   end
